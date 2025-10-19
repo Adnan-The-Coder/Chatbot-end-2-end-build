8d9eaa8bebec677427c808e5923211c318d1c903
Adnan <adnanali23189@gmail.com>
2025-10-19 08:54:23 +0530

    update cors

--- api.py
+++ api.py
@@ -12,6 +12,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000", 
         "https://datanyx-2025-website.vercel.app"             
+        "https://datanyx.in"
     ],
     allow_credentials=True,
     allow_methods=["*"],
